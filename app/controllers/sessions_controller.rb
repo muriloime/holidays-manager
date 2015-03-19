ecd167b920a8c98791075a091aa3dd7ddcd4a4a3
Ciro Chang <ciro.chang@studiare.com.br>
2015-03-19 18:29:22 -0300

    sessions_controler change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(login: params[:session][:login].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password_digest])
       # Log the user in and redirect to the user's show page.
       log_in(user)
       redirect_to user
