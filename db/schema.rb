bc2c4ea12e6647a6c218facf8078bb2706d8d333
Ciro Chang <ciro.chang@studiare.com.br>
2015-03-19 18:48:16 -0300

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150319205339) do
+ActiveRecord::Schema.define(version: 20150319214302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150319205339) do
     t.datetime "updated_at",                      null: false
     t.boolean  "manager",         default: false, null: false
     t.string   "password_digest"
+    t.string   "password"
   end
 
   add_index "users", ["login"], name: "index_users_on_login", unique: true, using: :btree
