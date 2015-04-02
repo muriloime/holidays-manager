01617f915e9bcd9b1843f23ca24fbd5d49c5a325
Ciro Chang <cirochang@live.com>
2015-04-02 11:07:46 -0300

    modify seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-User.create!([
+user = User.new([
   {login: "nathalia.casanova@studiare.com.br", name: "Nathalia Casanova", manager: false, password_digest: "$2a$10$kV7sv3W/pGGAB4cFf2aBMef4RMnVTNEsILIYdvAsj4mdlAstsLiIy", password: nil},
   {login: "rafael.zerbini@studiare.com.br", name: "Rafael Zerbini", manager: false, password_digest: "$2a$10$JnMrEMc5y20tromZugmucuCcmvjqeeVGMqYVQqbcYhiFLbQCzEdOK", password: nil},
   {login: "felipe.mattos@studiare.com.br", name: "Felipe Mattos", manager: true, password_digest: "$2a$10$0.SUjWQ7hj4UhfpRHNIUieVMaaplnRWzuq54TNxHT56Ei7ckO13XC", password: nil},
   {login: "murilo.andrade@studiare.com.br", name: "Murilo Andrade", manager: true, password_digest: "$2a$10$nb/KmrSl5Gl/8oRv24qpme3uRpfC9iuxJ9RQKyBzrkU8PYJ7N7VBq", password: nil},
   {login: "ciro.chang@studiare.com.br", name: "Ciro Chang", manager: false, password_digest: "$2a$10$p.9DaXdWdsVoVjAvDH7vZO.6jFkx32kwIgymYAIMMkktF6U8hd5pe", password: nil}
 ])
+user.save!
